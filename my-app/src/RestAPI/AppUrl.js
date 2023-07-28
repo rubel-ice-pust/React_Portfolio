d632b41f1d583e7962f069ddb2fefac57945e601
rubel-ice-pust <rubel.pust06@gmail.com>
2023-07-29 02:04:44 +0600

    GetDataWith Axios From Database

---
+++ my-app/src/RestAPI/AppUrl.js
@@ -0,0 +1,23 @@
+class AppUrl{
+
+    static BaseUrl= 'http://127.0.0.1:8000';
+
+    static HomeTopTitle = this.BaseUrl+'/HomeTopTitle';
+    static TechDesc = this.BaseUrl+'/TechDesc';
+    static TotallProjectClient = this.BaseUrl+'/TotallProjectClient';
+    static VideoHome = this.BaseUrl+'/VideoHome';
+    static ProjectDataDetails = this.BaseUrl+'/ProjectDataDetails';
+    static ProjectDataAll = this.BaseUrl+'/ProjectDataAll';
+    static ProjectData = this.BaseUrl+'/ProjectData';
+    static Services = this.BaseUrl+'/Services';
+    static Information = this.BaseUrl+'/Information';
+    static FooterData = this.BaseUrl+'/FooterData';
+    static CourseDetails = this.BaseUrl+'/CourseDetails';
+    static CourseAll = this.BaseUrl+'/CourseAll';
+    static CourseHome = this.BaseUrl+'/CourseHome';
+    static ContactSend = this.BaseUrl+'/ContactSend';
+    static ClientReview = this.BaseUrl+'/ClientReview';
+    static ChartData = this.BaseUrl+'/ChartData';
+    
+}
+export default AppUrl;
