8f3758fd4ca6f984e3cd11c6ef665fa96b88a878
rubel-ice-pust <rubel.pust06@gmail.com>
2023-08-02 01:01:46 +0600

    Service_Data_Get_From_Db

--- my-app/src/components/Services/Services.js
+++ my-app/src/components/Services/Services.js
@@ -1,45 +1,49 @@
 import React, { Component, Fragment} from 'react'
 import { Container,Row,Col } from 'react-bootstrap'
-import graphicLogo from '../../asset/image/graphics.svg'
-import mobileLogo from '../../asset/image/mobile.svg'
-import webLogo from '../../asset/image/web.svg'
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
 
 
 
 export default class Services extends Component {
+
+    constructor(){
+      super();
+      this.state={
+        myData:[]
+      }
+    }
+
+
+    componentDidMount(){
+      RestClient.GetRequest(AppUrl.Services).then(result=>{
+        this.setState({myData:result})
+
+      });
+    }
   render() {
+
+    const myList =this.state.myData;
+    const myView= myList.map(myList=>{
+
+      return   <Col lg={4} md={6} sm={12}>
+      <div className='serviceCard text-center'>
+          <img src={myList.service_logo}/>
+          <h2 className='serviceName'>{myList.service_name}</h2>
+          <p className='serviceDescription'>{myList.service_description}</p>
+      </div>
+  
+  </Col>
+    })
+
     return (
       <Fragment>
         <Container className='text-center' >
               <h1 className='serviceMainTitle'> MY SERVICES</h1>
             <Row>
 
-                <Col lg={4} md={6} sm={12}>
-                    <div className='serviceCard text-center'>
-                        <img src={webLogo}/>
-                        <h2 className='serviceName'>Web Development</h2>
-                        <p className='serviceDescription'>Web development refers to the creating, building, and maintaining of websites. It includes aspects such as web design, web publishing, web programming.</p>
-                    </div>
-                
-                </Col>
+                {myView}
 
-                <Col lg={4} md={6} sm={12}>
-                    <div className='serviceCard text-center'>
-                        <img src={mobileLogo}/>
-                        <h2 className='serviceName'>Mobile Development</h2>
-                        <p className='serviceDescription'>Mobile application development is the process of creating software applications that run on a mobile device, and a typical mobile application. </p>
-                    </div>
-                
-                </Col>
-
-                <Col lg={4} md={6} sm={12}>
-                   <div className='serviceCard text-center'>
-                        <img src={graphicLogo}/>
-                        <h2 className='serviceName'>Graphics Design</h2>
-                        <p className='serviceDescription'>Graphic design is the creation of visual compositions to solve problems and communicate ideas through typography, imagery, color and form.</p>
-                    </div>
-        
-                </Col>
             </Row>
         </Container>
       </Fragment>
