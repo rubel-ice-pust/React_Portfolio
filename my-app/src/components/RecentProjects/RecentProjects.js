8c864cbaf4a2089ae934a1930915046704757d97
rubel-ice-pust <rubel.pust06@gmail.com>
2023-08-03 02:24:12 +0600

    Get_Data From DB in COurse& Project Page

--- my-app/src/components/RecentProjects/RecentProjects.js
+++ my-app/src/components/RecentProjects/RecentProjects.js
@@ -1,59 +1,52 @@
 import React, { Component, Fragment } from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import webProject from '../../asset/image/WebProjects.png'
 import { Link } from 'react-router-dom';
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
 
 
 export default class RecentProjects extends Component {
+
+        constructor(){
+          super();
+          this.state={
+            myData:[]
+          }
+        }
+
+
+        componentDidMount(){
+          RestClient.GetRequest(AppUrl.ProjectData).then(result=>{
+            this.setState({myData:result})
+
+          });
+        }
+
   render() {
+    
+    const myList =this.state.myData;
+    const myView= myList.map(myList=>{
+
+      return     <Col sm={12} md={6} lg={4} className='p-2'>
+            <Card className='projectCard'>
+                <Card.Img variant="top" src={myList.img_one} />
+                <Card.Body>
+                    <Card.Title className='projectCardTitle'>{myList.project_name}</Card.Title>
+                    <Card.Text className='projectCardDes'>{myList.short_description}</Card.Text>
+                    <Button variant="primary"><Link className='link-style' to="/ProjectDetails">Details</Link></Button>
+                </Card.Body>
+          </Card>
+
+            </Col>
+    })
     return (
       <Fragment>
         <Container className='text-center'>
            <h1 className='serviceMainTitle'> RECENT PROJECTS</h1>
             <Row>
 
-                <Col sm={12} md={6} lg={4} className='p-2'>
-                <Card className='projectCard'>
-                    <Card.Img variant="top" src={webProject} />
-                    <Card.Body>
-                        <Card.Title className='projectCardTitle'>Card Title</Card.Title>
-                        <Card.Text className='projectCardDes'>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary"><Link className='link-style' to="/ProjectDetails">Details</Link></Button>
-                    </Card.Body>
-              </Card>
-
-                </Col>
-                <Col sm={12} md={6} lg={4} className='p-2'>
-                <Card className='projectCard'>
-                    <Card.Img variant="top" src={webProject} />
-                    <Card.Body>
-                        <Card.Title className='projectCardTitle'>Card Title</Card.Title>
-                        <Card.Text className='projectCardDes'>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary"> <Link className='link-style'  to="/ProjectDetails">Details</Link> </Button>
-                    </Card.Body>
-              </Card>
-                
-                </Col>
-                <Col sm={12} md={6} lg={4} className='p-2'>
-                <Card className='projectCard'>
-                    <Card.Img variant="top" src={webProject} />
-                    <Card.Body>
-                        <Card.Title className='projectCardTitle'>Card Title</Card.Title>
-                        <Card.Text className='projectCardDes'>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary"> <Link className='link-style' to="/ProjectDetails">Details</Link> </Button>
-                    </Card.Body>
-              </Card>
-                
-                </Col>
+               {myView}
+                    
             </Row>
         </Container>
       </Fragment>
